5b914f5c887b55967b76f9903cccdb0359699178
Maxime Verreault <maximeverreault24@gmail.com>
2020-10-02 16:29:57 -0400

    ASUQTR-484 - Added basic AUV display for the 8 motors as a SVG. Added 1 motor indicator as an arrow (green for forward/top, red for backward/bottom) which fade in the more the thruster request is high and fade out the more the thruster request is low. #time 1d

--- src/app/auv-motor-display/auv-motor-display.component.ts
+++ src/app/auv-motor-display/auv-motor-display.component.ts
@@ -1,21 +1,94 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+    animate,
+    state,
+    style,
+    transition,
+    trigger,
+} from '@angular/animations';
+import { GamepadService } from '../gamepad.service';
 
 @Component({
     selector: 'app-auv-motor-display',
     templateUrl: './auv-motor-display.component.html',
     styleUrls: ['./auv-motor-display.component.scss'],
+    animations: [
+        trigger('goFast', [
+            state(
+                'start',
+                style({
+                    'stroke-dashoffset': 0,
+                })
+            ),
+            state(
+                'end',
+                style({
+                    'stroke-dashoffset': '{{ animationSpeed }}',
+                }),
+                { params: { animationSpeed: '200' } }
+            ),
+            transition('start => end', [animate('2s')]),
+            transition('end => start', [animate('2s')]),
+        ]),
+    ],
 })
-export class AuvMotorDisplayComponent implements OnInit {
-    // @ViewChild('canvas', { static: true })
-    // canvas: ElementRef<HTMLCanvasElement>;
-    // private ctx: CanvasRenderingContext2D;
-    constructor() {}
+export class AuvMotorDisplayComponent implements OnInit, OnDestroy {
+    isActive = false;
+    animationSpeed: number;
+    gamepadConnectedSubscription: any;
+    gamepadDisconnectedSubscription: any;
+    gamepads: Gamepad[];
+    connected: boolean;
+    startTime: number;
+    offset: number;
+    intensityForward = 0;
+    intensityBackward = 0;
+
+    constructor(public gs: GamepadService) {}
 
     ngOnInit(): void {
-        // this.ctx = this.canvas.nativeElement.getContext('2d');
-        // const path = new Path2D(
-        //     'm5.23252,67.31465l110.71307,-62.67882l156.57388,0l110.71307,62.67882l0,88.64236l-110.71307,62.67883l-156.57388,0l-110.71307,-62.67883l0,-88.64236z'
-        // );
-        // this.ctx.stroke(path);
+        this.gamepadConnectedSubscription = this.gs.onGamepadConnected.subscribe(
+            (e: GamepadEvent) => {
+                if (e) {
+                    this.connected = true;
+                    this.componentDidMount();
+                }
+            }
+        );
+        this.gamepadDisconnectedSubscription = this.gs.onGamepadDisconnected.subscribe(
+            (e: GamepadEvent) => {
+                if (e) {
+                    this.connected = false;
+                }
+            }
+        );
+    }
+
+    componentDidMount() {
+        this.startTime = performance.now();
+        this.tick();
+    }
+
+    tick() {
+        this.gamepads = this.pollGamepads();
+        const intensity = this.gamepads[0].axes[0];
+        this.intensityForward = intensity > 0 ? intensity : 0;
+        this.intensityBackward = intensity < 0 ? intensity * -1 : 0;
+        // const timePassed = performance.now() - this.startTime;
+        // const progress = (timePassed % 2000) / 2000;
+        // this.offset = progress * 500 * intensity;
+        // console.log(this.offset);
+        if (this.connected) {
+            window.requestAnimationFrame(() => this.tick());
+        }
+    }
+
+    pollGamepads() {
+        return navigator.getGamepads();
+    }
+
+    ngOnDestroy(): void {
+        this.gamepadConnectedSubscription.unsubscribe();
+        this.gamepadDisconnectedSubscription.unsubscribe();
     }
 }
