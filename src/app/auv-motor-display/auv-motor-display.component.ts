d2bf8d2f8a64f5529dbf55542258b2e9aed66915
Maxime Verreault <maximeverreault24@gmail.com>
2020-10-11 16:39:31 -0400

    ASUQTR-536 - Updated intensity of representation for motor throttles #time 30m

--- src/app/auv-motor-display/auv-motor-display.component.ts
+++ src/app/auv-motor-display/auv-motor-display.component.ts
@@ -47,12 +47,12 @@ export class AuvMotorDisplayComponent implements OnInit, OnDestroy {
      * @param motorThrottles Motors topic data coming from the RosService
      */
     extractMotorThrottlesData(motorThrottles: MotorThrottlesMessage): void {
-        for (let index = 0; index < motorThrottles.throttles.length; index ++) {
+        for (let index = 0; index < motorThrottles.throttles.length; index++) {
             const intensity = motorThrottles.throttles[index];
             this.motors[index].forwardIntensity = intensity > 0 ? intensity : 0;
             this.motors[index].backwardIntensity =
                 intensity < 0 ? intensity * -1 : 0;
-            this.motors[index].fillOpacity = Math.abs(intensity);
+            this.motors[index].fillOpacity = (Math.abs(intensity) * 4) / 3;
         }
     }
 
