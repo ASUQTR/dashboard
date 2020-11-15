ea14e1d30e56ce950e06bcf9eda432c20d8b2f89
Maxime Verreault <maximeverreault24@gmail.com>
2020-11-15 15:06:35 -0500

    Fixed import

--- src/app/control-toggle/control-toggle.component.ts
+++ src/app/control-toggle/control-toggle.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
 import { RosService } from '../ros.service';
-import { ControlEnableMessage } from '../ros-model.enum';
 
 @Component({
     selector: 'app-control-toggle',
