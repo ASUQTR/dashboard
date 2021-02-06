3d808c75428d341a69d2781330813d6ce2a5729e
Maxime Verreault <maximeverreault24@gmail.com>
2021-02-06 15:51:28 -0500

    ASUQTR-938 - Fixed current unit tests #time 1h

--- src/app/debug/control-lqr-debug-info/control-lqr-debug-info.component.spec.ts
+++ src/app/debug/control-lqr-debug-info/control-lqr-debug-info.component.spec.ts
@@ -1,25 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ControlLqrDebugInfoComponent } from './control-lqr-debug-info.component';
+import { NbLayoutModule, NbThemeModule, NbToastrModule } from '@nebular/theme';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ControlLqrDebugInfoComponent', () => {
-  let component: ControlLqrDebugInfoComponent;
-  let fixture: ComponentFixture<ControlLqrDebugInfoComponent>;
+    let component: ControlLqrDebugInfoComponent;
+    let fixture: ComponentFixture<ControlLqrDebugInfoComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ControlLqrDebugInfoComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ControlLqrDebugInfoComponent],
+            imports: [
+                NbToastrModule.forRoot(),
+                NbThemeModule.forRoot(),
+                NbLayoutModule,
+                HttpClientModule,
+            ],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ControlLqrDebugInfoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ControlLqrDebugInfoComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/flexbe-behavior-killswitch/flexbe-behavior-killswitch.component.spec.ts
+++ src/app/flexbe-behavior-killswitch/flexbe-behavior-killswitch.component.spec.ts
@@ -1,25 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FlexbeBehaviorKillswitchComponent } from './flexbe-behavior-killswitch.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NbLayoutModule, NbThemeModule, NbToastrModule } from '@nebular/theme';
 
 describe('FlexbeBehaviorKillswitchComponent', () => {
-  let component: FlexbeBehaviorKillswitchComponent;
-  let fixture: ComponentFixture<FlexbeBehaviorKillswitchComponent>;
+    let component: FlexbeBehaviorKillswitchComponent;
+    let fixture: ComponentFixture<FlexbeBehaviorKillswitchComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FlexbeBehaviorKillswitchComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [FlexbeBehaviorKillswitchComponent],
+            imports: [
+                HttpClientModule,
+                NbToastrModule.forRoot(),
+                NbThemeModule.forRoot(),
+                NbLayoutModule,
+            ],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FlexbeBehaviorKillswitchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FlexbeBehaviorKillswitchComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
