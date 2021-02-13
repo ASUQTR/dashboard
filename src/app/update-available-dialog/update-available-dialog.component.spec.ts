44e4f8d07560bb0e15739af4ab6dd47eec289c85
Maxime Verreault <maximeverreault24@gmail.com>
2021-02-12 21:03:09 -0500

    ASUQTR-966 - Fixed unit tests #time 30m

--- src/app/update-available-dialog/update-available-dialog.component.spec.ts
+++ src/app/update-available-dialog/update-available-dialog.component.spec.ts
@@ -1,25 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UpdateAvailableDialogComponent } from './update-available-dialog.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NbLayoutModule, NbThemeModule, NbToastrModule } from '@nebular/theme';
 
 describe('UpdateAvailableDialogComponent', () => {
-  let component: UpdateAvailableDialogComponent;
-  let fixture: ComponentFixture<UpdateAvailableDialogComponent>;
+    let component: UpdateAvailableDialogComponent;
+    let fixture: ComponentFixture<UpdateAvailableDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UpdateAvailableDialogComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [UpdateAvailableDialogComponent],
+            imports: [
+                HttpClientModule,
+                NbToastrModule.forRoot(),
+                NbThemeModule.forRoot(),
+                NbLayoutModule,
+            ],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UpdateAvailableDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UpdateAvailableDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
