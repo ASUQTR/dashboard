2f33226762b3a9943621ed221845c62a13ffedae
Gabriel Tremblay <tue.mouche05@gmail.com>
2021-06-05 10:55:39 -0700

    oldschool gamepad web interface for times where ASUQTR Dashboard causes faults/bugs for testing of other modules, like control.

--- src/app/true-false.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TrueFalsePipe } from './true-false.pipe';
-
-describe('TrueFalsePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TrueFalsePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
