fe86c017647d6edd5807d05b80acb3262e56cad9
Maxime Verreault <maximeverreault24@gmail.com>
2021-09-04 15:48:53 -0400

    ASUQTR-1222 - Fixed tests #time 30m

--- src/app/nav-get-position/nav-get-position.component.spec.ts
+++ src/app/nav-get-position/nav-get-position.component.spec.ts
@@ -1,25 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavGetPositionComponent } from './nav-get-position.component';
+import { NbLayoutModule, NbThemeModule, NbToastrModule } from '@nebular/theme';
+import { Vector3dPipe } from '../vector3d.pipe';
 
 describe('NavGetPositionComponent', () => {
-  let component: NavGetPositionComponent;
-  let fixture: ComponentFixture<NavGetPositionComponent>;
+    let component: NavGetPositionComponent;
+    let fixture: ComponentFixture<NavGetPositionComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NavGetPositionComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [NavGetPositionComponent, Vector3dPipe],
+            imports: [NbToastrModule.forRoot(), NbThemeModule.forRoot(), NbLayoutModule],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NavGetPositionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NavGetPositionComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/nav-reposition/nav-reposition.component.spec.ts
+++ src/app/nav-reposition/nav-reposition.component.spec.ts
@@ -1,25 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavRepositionComponent } from './nav-reposition.component';
+import { NbLayoutModule, NbThemeModule, NbToastrModule } from '@nebular/theme';
 
 describe('NavRepositionComponent', () => {
-  let component: NavRepositionComponent;
-  let fixture: ComponentFixture<NavRepositionComponent>;
+    let component: NavRepositionComponent;
+    let fixture: ComponentFixture<NavRepositionComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NavRepositionComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [NavRepositionComponent],
+            imports: [NbToastrModule.forRoot(), NbThemeModule.forRoot(), NbLayoutModule],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NavRepositionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NavRepositionComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
